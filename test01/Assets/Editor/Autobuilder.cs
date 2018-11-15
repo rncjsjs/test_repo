39b76152b5c6f2bf95e55ca1c74704274e05f315
silva <silva@supernova11.com>
2018-11-15 18:39:23 +0900

    AutoBuilder.cs 수정

---
+++ test01/Assets/Editor/Autobuilder.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using UnityEditor;
+
+public class Autobuilder : MonoBehaviour
+{
+
+    public static void BuildGame()
+    {
+
+        var scenes = new string[] { "Assets/Scenes/SampleScene.unity" };
+        var flags = BuildOptions.Development;
+        Debug.Log("start");
+        BuildPipeline.BuildPlayer(scenes, "test2.apk", BuildTarget.Android, flags);
+        Debug.Log("end");
+
+    }
+
+}
---
+++ test01/Assets/Editor/MenuItems.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class MenuItems
+{
+    [MenuItem("Tools/Autobuilder")]
+    private static void NewMenuOption()
+    {
+        Autobuilder.BuildGame();
+    }
+}
