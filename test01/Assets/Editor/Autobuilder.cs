7cfe1da23300ff5480b0874e233183d38c00342b
silva <silva@supernova11.com>
2018-11-16 14:29:52 +0900

    Autobuilder.cs

--- test01/Assets/Editor/Autobuilder.cs
+++ test01/Assets/Editor/Autobuilder.cs
@@ -13,7 +13,7 @@ public class Autobuilder : MonoBehaviour
         var scenes = new string[] { "Assets/Scenes/SampleScene.unity" };
         var flags = BuildOptions.Development;
         Debug.Log("start");
-        BuildPipeline.BuildPlayer(scenes, "test2.apk", BuildTarget.Android, flags);
+        BuildPipeline.BuildPlayer(scenes, "d:\test2.apk", BuildTarget.Android, flags);
         Debug.Log("end");
 
     }
